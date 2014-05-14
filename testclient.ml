b3c67e0ee0d8e4f234b1d0f6263819a5ddc689dc
Elie Michel <elie.michel@ens.fr>
2014-05-14 18:28:16 +0200

    Client mis à jour. Là, on a en gros un chat opérationnel

--- testclient.ml
+++ testclient.ml
@@ -4,14 +4,26 @@ open Handcut
 let () =
 	let servout, servin = open_connection master_addr in
 		print_endline "Connected.";
-		let rec repl () =
+		let run = ref true in
+
+		let rec from_server () =
+			print_endline (input_line servout);
+			if !run then from_server ()
+		in
+
+		let rec from_user () =
 			print_string "> ";
 			let l = read_line () in
 				output_line servin l;
-				print_endline (input_line servout);
-				if String.compare l "quit" != 0 then repl ()
-		in repl ();
-		shutdown_connection servout;
-		print_endline "Disconnected."
+				if String.compare l "quit" != 0
+				then from_user ()
+				else run := false
+		in
+
+		let th = Thread.create from_server () in
+			from_user ();
+			Thread.join th;
+			shutdown_connection servout;
+			print_endline "Disconnected."
 
 
